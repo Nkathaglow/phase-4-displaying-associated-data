0f16bd80ad967ce90e72c134406c55fb0b1032aa
Nkatha_Glory <kinyua6945@gmail.com>
2023-06-22 09:43:35 +0300

    Done

--- app/controllers/dog_houses_controller.rb
+++ app/controllers/dog_houses_controller.rb
@@ -9,7 +9,8 @@ class DogHousesController < ApplicationController
   private
 
   def render_not_found_response
-    render json: { error: "Dog house not found" }, status: :not_found
+    render json: dog_house, include: :reviews
+
   end
 
 end
